b34db0e47660b3f435be878573b1fefa241b2cbe
maxwel2242-byte <maxwel2242@gmail.com>
2026-05-08 23:54:54 +0200

    adjust navigatefallback

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
     },
     workbox: {
       globPatterns: ['**/*.{js,css,html,png,ico,svg}'],
-      navigateFallback: '/',
+      navigateFallback: null,
     },
     devOptions: {
       enabled: true,
